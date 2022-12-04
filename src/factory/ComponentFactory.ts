36f71f9150be6dd7627b7c952c10da4da09903bc
Leonardodsch <leo.scheuermann@gmail.com>
2022-12-04 00:52:15 -0300

    Update ComponentFactory.ts
    component factory updated

--- src/factory/ComponentFactory.ts
+++ src/factory/ComponentFactory.ts
@@ -1,12 +1,17 @@
 import IComponent from '../contract/IComponent';
 import TextComponent from '../component/TextComponent';
+import IntegerComponent from '../component/IntegerComponent';
+import DoubleComponent from '../component/DoubleComponent';
 
 export default class ComponentFactory {
   private static types: { [_: string]: () => IComponent } = {
-    "string" : () => new TextComponent()
+    "string": () => new TextComponent(),
+    "integer": () => new IntegerComponent(),
+    "double": () => new DoubleComponent()
   };
 
   public static create(typeName: string): IComponent {
     return this.types[typeName]();
   }
 }
+
